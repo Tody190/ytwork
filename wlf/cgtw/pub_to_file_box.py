aead7cfa17ed62aee22c91df05edb707707cf1e7
yangtao <toddyyoung@foxmail.com>
2023-12-15 15:33:52 +0800

    第一次提交

---
+++ wlf/cgtw/pub_to_file_box.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/11 14:29
+# @Author  : YangTao
+# @File    : pub_to_file_box.py
+
+
+def pub():
+    pass
+
+if __name__ == '__main__':
+    project_name = "test"
+    module = "shot"
